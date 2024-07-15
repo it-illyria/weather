126330b9a7b23904301071858d2774af44e396a1
jlika <similika25@gmail.com>
2024-07-15 23:50:47 +0200

    images commit

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-weather_project/
-├── weather_project/
-│   ├── settings.py
-│   ├── urls.py
-│   └── ...
-├── weather/
-│   ├── __init__.py
-│   │── fetch_weather_data.py  
-│   ├── models.py
-│   ├── views.py
-│   └── ...
-└── manage.py
+# Weather Forecast Demo
--- weather/views.py
+++ weather/views.py
@@ -6,7 +6,7 @@ from .service import fetch_weather_data
 def weather_view(request):
     fetch_weather_data()
 
-    weather_data = WeatherData.objects.order_by('-timestamp')[:12].values()
+    weather_data = WeatherData.objects.order_by('-timestamp')[:13].values()
     context = {
         'weather_data': weather_data
     }
